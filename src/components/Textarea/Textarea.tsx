2423cc0946467d4ba390275f095dd03718ff34b0
lkeix <ai.kanaria.ai@gmail.com>
2022-06-05 20:28:34 +0900

    add: Textarea

---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  resize: none;
+  font-size: 16px;
+  line-height: 1.5;
+  padding: 8px 16px;
+  box-sizing: border-box;
+`
